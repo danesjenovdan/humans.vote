390f53180982e375f237dc1b4d96ffcbf004556f
mzgajner <mato@zgajner.com>
2018-01-22 20:57:58 +0100

    Add voter web client interface boilerplate

---
+++ voter-client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <h1 class="display-4">{{ title }}</h1>
+        <v-btn color="primary">Vote yes</v-btn>
+        <v-btn color="error">Vote no</v-btn>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Humans Vote',
+    }
+  }
+}
+</script>
