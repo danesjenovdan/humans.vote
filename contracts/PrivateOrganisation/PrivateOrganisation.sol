3f48ef5ea22bd09947d00829bcd5be924e2103c6
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-01-23 14:10:50 +0100

    finish work on PrivateOrganisation.sol

--- contracts/PrivateOrganisation/PrivateOrganisation.sol
+++ contracts/PrivateOrganisation/PrivateOrganisation.sol
@@ -20,11 +20,6 @@ contract Owned {
 }
 
 
-interface Token {
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-}
-
-
 contract PrivateOrganisation is Owned {
     // Contract Variables and events
     uint public minimumQuorum; // minimum quorum defined as an absolute number of voters
@@ -49,7 +44,6 @@ contract PrivateOrganisation is Owned {
         bool proposalPassed;
         uint numberOfVotes;
         int currentResult;
-        bytes32 proposalHash;
         Vote[] votes; // dynamic array of Vote objects
         mapping (address => bool) voted; // mapping of addresses to store who voted
     }
@@ -63,12 +57,13 @@ contract PrivateOrganisation is Owned {
 
     // vote "object" structure
     struct Vote {
-        bool inSupport;
-        address voter;
-        string justification;
+        bool inSupport; // is the vote for or against?
+        address voter; // who voted
+        string justification; // justification string
     }
 
     // Modifier that allows only shareholders to vote and create new proposals
+    // msg.sender needs to have non-zero value in memberId mapping
     modifier onlyMembers {
         require(memberId[msg.sender] != 0);
         _;
@@ -76,13 +71,14 @@ contract PrivateOrganisation is Owned {
 
     /**
      * Constructor function
+     * Needs minimumQuorumForProposals, minutesForDevate, marginOfVotesForMajority
      */
     function PrivateOrganisation (
         uint minimumQuorumForProposals,
         uint minutesForDebate,
         int marginOfVotesForMajority
-    ) public payable {
-        // set voting rules
+    ) public {
+        // set voting rules at contract initialisation
         changeVotingRules(minimumQuorumForProposals, minutesForDebate, marginOfVotesForMajority);
 
         // It’s necessary to add an empty first member
@@ -93,21 +89,27 @@ contract PrivateOrganisation is Owned {
     }
 
     /**
-     * Add member
+     * Add member function
      *
      * Make `targetMember` a member named `memberName`
      *
      * @param targetMember ethereum address to be added
      * @param memberName public name for that member
+     * 
+     * modified with onlyOwner
      */
     function addMember(address targetMember, string memberName) public onlyOwner {
+        // get member id
         uint id = memberId[targetMember];
+        // if member does not exist add member
         if (id == 0) {
             memberId[targetMember] = members.length;
             id = members.length++;
         }
 
+        // add this specific Member to members
         members[id] = Member({member: targetMember, memberSince: now, name: memberName});
+        // emit MembershipChanged event
         MembershipChanged(targetMember, true);
     }
 
@@ -117,14 +119,20 @@ contract PrivateOrganisation is Owned {
      * @notice Remove membership from `targetMember`
      *
      * @param targetMember ethereum address to be removed
+     *
+     * modified with onlyOwner
      */
     function removeMember(address targetMember) public onlyOwner {
+        // make sure member actually exists before removing
         require(memberId[targetMember] != 0);
 
+        // move member to the end of the members array STRANGE
         for (uint i = memberId[targetMember]; i < members.length-1; i++) {
             members[i] = members[i+1];
         }
+        // delete members
         delete members[members.length-1];
+        // decrease members.length by 1
         members.length--;
     }
 
@@ -137,6 +145,8 @@ contract PrivateOrganisation is Owned {
      * @param minimumQuorumForProposals how many members must vote on a proposal for it to be executed
      * @param minutesForDebate the minimum amount of delay between when a proposal is made and when it can be executed
      * @param marginOfVotesForMajority the proposal needs to have 50% plus this number
+     *
+     * modified with onlyOwner
      */
     function changeVotingRules(
         uint minimumQuorumForProposals,
@@ -154,82 +164,23 @@ contract PrivateOrganisation is Owned {
     /**
      * Add Proposal
      *
-     * Propose to send `weiAmount / 1e18` ether to `beneficiary` for `jobDescription`. 
-     * `transactionBytecode ? Contains : Does not contain` code.
+     * Propose a text thing
      *
-     * @param beneficiary who to send the ether to
-     * @param weiAmount amount of ether to send, in wei
-     * @param jobDescription Description of job
-     * @param transactionBytecode bytecode of transaction
+     * @param proposalText the text of the proposal
+     *
+     * modified by onlyMembers
      */
-    function newProposal(
-        address beneficiary,
-        uint weiAmount,
-        string jobDescription,
-        bytes transactionBytecode
-    )
-        public onlyMembers
-        returns (uint proposalID)
-    {
-        proposalID = proposals.length++;
-        Proposal storage p = proposals[proposalID];
-        p.recipient = beneficiary;
-        p.amount = weiAmount;
-        p.description = jobDescription;
-        p.proposalHash = keccak256(beneficiary, weiAmount, transactionBytecode);
-        p.votingDeadline = now + debatingPeriodInMinutes * 1 minutes;
-        // p.executed = false;
-        p.proposalPassed = false;
-        p.numberOfVotes = 0;
-        ProposalAdded(proposalID, beneficiary, weiAmount, jobDescription);
-        numProposals = proposalID+1;
+    function newProposal(string proposalText) public onlyMembers returns (uint proposalID) {
+        proposalID = proposals.length++; // proposalID is next proposal (autoincrement)
+        Proposal storage p = proposals[proposalID]; // get Proposal
+        p.description = proposalText; // set proposalText
+        p.votingDeadline = now + debatingPeriodInMinutes * 1 minutes; // set debating period
+        p.proposalPassed = false; // didn't pass when created
+        p.numberOfVotes = 0; // 0 votes when created
+        ProposalAdded(proposalID, proposalText); // trigger ProposalAdded event
+        numProposals = proposalID+1; // increment number of proposals
 
         return proposalID;
-    }
-
-    /**
-     * Add proposal in Ether
-     *
-     * Propose to send `etherAmount` ether to `beneficiary` for `jobDescription`. `transactionBytecode ?
-     * Contains : Does not contain` code. This is a convenience function to use if the amount to be given is
-     * in round number of ether units.
-     *
-     * @param beneficiary who to send the ether to
-     * @param etherAmount amount of ether to send
-     * @param jobDescription Description of job
-     * @param transactionBytecode bytecode of transaction
-     */
-    function newProposalInEther(
-        address beneficiary,
-        uint etherAmount,
-        string jobDescription,
-        bytes transactionBytecode
-    )
-        public onlyMembers
-        returns (uint proposalID)
-    {
-        return newProposal(beneficiary, etherAmount * 1 ether, jobDescription, transactionBytecode);
-    }
-
-    /**
-     * Check if a proposal code matches
-     *
-     * @param proposalNumber ID number of the proposal to query
-     * @param beneficiary who to send the ether to
-     * @param weiAmount amount of ether to send
-     * @param transactionBytecode bytecode of transaction
-     */
-    function checkProposalCode(
-        uint proposalNumber,
-        address beneficiary,
-        uint weiAmount,
-        bytes transactionBytecode
-    )
-        public constant
-        returns (bool codeChecksOut)
-    {
-        Proposal storage p = proposals[proposalNumber];
-        return p.proposalHash == keccak256(beneficiary, weiAmount, transactionBytecode);
     }
 
     /**
@@ -240,6 +191,8 @@ contract PrivateOrganisation is Owned {
      * @param proposalNumber number of proposal
      * @param supportsProposal either in favor or against it
      * @param justificationText optional justification text
+     *
+     * modified by onlyMembers
      */
     function vote(
         uint proposalNumber,
@@ -249,17 +202,17 @@ contract PrivateOrganisation is Owned {
         public onlyMembers
         returns (uint voteID)
     {
-        Proposals storage p = proposals[proposalNumber];         // Get the proposal
-        require(!p.voted[msg.sender]);         // If has already voted, cancel
-        p.voted[msg.sender] = true;                     // Set this voter as having voted
-        p.numberOfVotes++;                              // Increase the number of votes
-        if (supportsProposal) {                         // If they support the proposal
-            p.currentResult++;                          // Increase score
-        } else {                                        // If they don't
-            p.currentResult--;                          // Decrease the score
+        Proposal storage p = proposals[proposalNumber]; // Get the proposal
+        require(!p.voted[msg.sender]); // If msg.sender has already voted, cancel
+        p.voted[msg.sender] = true; // Set this voter as having voted
+        p.numberOfVotes++; // Increase the number of votes
+        if (supportsProposal) { // If they support the proposal
+            p.currentResult++; // Increase score
+        } else { // If they don't
+            p.currentResult--; // Decrease the score
         }
 
-        // Create a log of this event
+        // Create a log of this event (emit so app can update)
         Voted(proposalNumber, supportsProposal, msg.sender, justificationText);
         return p.numberOfVotes;
     }
@@ -270,24 +223,17 @@ contract PrivateOrganisation is Owned {
      * Count the votes proposal #`proposalNumber` and execute it if approved
      *
      * @param proposalNumber proposal number
-     * @param transactionBytecode optional: if the transaction contained a bytecode, you need to send it
+     * 
      */
-    function executeProposal(uint proposalNumber, bytes transactionBytecode) public {
-        Proposal storage p = proposals[proposalNumber];
+    function getProposalResults(uint proposalNumber) public {
+        Proposal storage p = proposals[proposalNumber]; // get proposal
 
-        require(now > p.votingDeadline                                            // If it is past the voting deadline
-            // && !p.executed                                                         // and it has not already been executed
-            // && p.proposalHash == keccak256(p.recipient, p.amount, transactionBytecode)  // and the supplied code matches the proposal
-            && p.numberOfVotes >= minimumQuorum);                                  // and a minimum quorum has been reached...
+        require(now > p.votingDeadline // If it is past the voting deadline
+            && p.numberOfVotes >= minimumQuorum); // and a minimum quorum has been reached...
 
         // ...then execute result
-
         if (p.currentResult > majorityMargin) {
-            // Proposal passed; execute the transaction
-
-            // p.executed = true; // Avoid recursive calling
-            // require(p.recipient.call.value(p.amount)(transactionBytecode));
-
+            // Proposal passed
             p.proposalPassed = true;
         } else {
             // Proposal failed
