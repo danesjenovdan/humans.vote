98a0e9f797892034d2e2417cd0c9ec0e5803cd1d
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-01-23 14:48:42 +0100

    extracted Owned.sol to utils

--- contracts/PrivateOrganisation/PrivateOrganisation.sol
+++ contracts/PrivateOrganisation/PrivateOrganisation.sol
@@ -1,23 +1,7 @@
 pragma solidity ^0.4.18;
 
-
-// Owned contracts are owned by a single address, and owner has rights issued through onlyOwner modifier
-contract Owned {
-    address public owner;
-
-    function Owned()  public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        owner = newOwner;
-    }
-}
+// import Owned
+import "../utils/Owned.sol";
 
 
 contract PrivateOrganisation is Owned {
