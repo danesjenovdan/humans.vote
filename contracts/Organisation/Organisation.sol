fb1d4ec8300108b37b85e0aac8675bb760468426
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-01-23 21:38:58 +0100

    base organisation contract

---
+++ contracts/Organisation/Organisation.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.18;
+
+import "../utils/PrivateOrg.sol";
+
+
+contract Organisation is PrivateOrg {
+    // Contract Variables and events
+    string public organisationName;
+
+    event OrganisationNameChanged(string organisationName);
+
+    /**
+     * Constructor function
+     * Needs _organisationName
+     */
+    function Organisation (
+        string _organisationName
+    ) public {
+        // set voting rules at contract initialisation
+        organisationName = _organisationName;
+    }
+
+    /**
+     * Change organisation name
+     * 
+     * takes newOrganisationName
+     * 
+     * modified with onlyOwner
+     */
+    function changeOrganisationName(
+        string newOrganisationName
+    ) public onlyOwner {
+        organisationName = newOrganisationName;
+
+        // fire ChangeOfRules event
+        OrganisationNameChanged(newOrganisationName);
+    }
+}
+
