94542aededc819ef3b57c8f9c786fcddae1db19d
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-01-24 15:08:17 +0100

    contract v1

---
+++ contracts/MotionVotingOrganisation/MotionVotingOrganisation.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.18;
+
+import "../utils/PrivateOrg.sol";
+import "../utils/Motion.sol";
+
+
+contract MotionVotingOrganisation is PrivateOrg {
+    // Contract Variables and events
+    string public organisationName;
+    
+
+    event OrganisationNameChanged(string organisationName);
+
+    /**
+     * Constructor function
+     * Needs _organisationName
+     */
+    function MotionVotingOrganisation (
+        string _organisationName
+    ) public {
+        // set voting rules at contract initialisation
+        organisationName = _organisationName;
+    }
+
+    /**
+     * Change organisation name
+     * 
+     * takes newOrganisationName
+     * 
+     * modified with onlyOwner
+     */
+    function changeOrganisationName(
+        string newOrganisationName
+    ) public onlyOwner {
+        organisationName = newOrganisationName;
+
+        // fire ChangeOfRules event
+        OrganisationNameChanged(newOrganisationName);
+    }
+
+    
+}
